fdf93f1febbf182d4877e0f0e0c4a0c00b32c8a7
Trengland <englandvivial@gmail.com>
2023-04-13 22:53:05 -0400

    arranged folder to read html properly

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
 const express = require('express');
 const apiroutes = require('./routes/apiroutes');
 const htmlroutes = require('./routes/htmlroutes');
-// const htmlroutes = ('./routes/htmlroutes');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const logRequest = (req, res, next) => {
-    console.log(`${req.method} ${req.path}`);
-    next();
-  };
- 
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
 app.use('/api', apiroutes);
-
-app.use(logRequest);
 
 app.use('/', htmlroutes);
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
